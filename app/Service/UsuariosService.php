ea1c37271f066f4a711ac1e05e9a83e78b2e7982
noziXsHooter <nozixco@gmail.com>
2023-06-11 18:59:49 -0300

    add cadastrar() method and setDadosCorpoRequest

--- app/Service/UsuariosService.php
+++ app/Service/UsuariosService.php
@@ -11,8 +11,10 @@ class UsuariosService {
     const TABELA = 'usuarios';
     const RECURSOS_GET = ['listar'];
     const RECURSOS_DELETE = ['deletar'];
+    const RECURSOS_POST = ['cadastrar'];
 
     private array $dados;
+    private array $dadosCorpoRequest = [];
 
     /**
      * @var object|UsuariosRepository
@@ -49,6 +51,26 @@ class UsuariosService {
         return $retorno;
     }
 
+    public function validarPost()
+    {
+
+        $retorno = null;
+        $recurso = $this->dados['recurso'];
+
+        if(in_array($recurso, self::RECURSOS_POST, true)) {
+            $retorno = $this->$recurso();
+        }else {
+            throw new InvalidArgumentException(ConstantesGenericasUtil::MSG_ERRO_RECURSO_INEXISTENTE);
+        }
+
+        if($retorno === null)
+        {
+            throw new InvalidArgumentException(ConstantesGenericasUtil::MSG_ERRO_GENERICO);
+        }
+
+        return $retorno;
+    }
+
     public function validarDelete()
     {
 
@@ -71,6 +93,11 @@ class UsuariosService {
         }
     }
 
+    public function setDadosCorpoRequest($dadosRequest)
+    {
+        $this->dadosCorpoRequest = $dadosRequest;
+    }
+
     public function getOneByKey()
     {
         return $this->UsuariosRepository->getMySQL()->getOneByKey(self::TABELA, $this->dados['id']);
@@ -79,6 +106,24 @@ class UsuariosService {
     private function listar()
     {
         return $this->UsuariosRepository->getMySQL()->getAll(self::TABELA);
+    }
+
+    private function cadastrar()
+    {
+        [$login, $senha] = [$this->dadosCorpoRequest['login'], $this->dadosCorpoRequest['senha']];
+        if($login && $senha) {
+            if ($this->UsuariosRepository->insertUser($login, $senha) > 0) {
+                $idInserido = $this->UsuariosRepository->getMySQL()->getDb()->lastInsertId();
+                $this->UsuariosRepository->getMySQL()->getDb()->commit();
+
+                return ['id' => $idInserido];
+            }else{
+                $this->UsuariosRepository->getMysql()->getDb()->rollBack();// Desfaz a transação em caso de erro
+                throw new InvalidArgumentException(ConstantesGenericasUtil::MSG_ERRO_GENERICO);
+            }
+        } else {
+            throw new InvalidArgumentException(ConstantesGenericasUtil::MSG_ERRO_LOGIN_SENHA_OBRIGATORIO);
+        }
     }
 
     private function deletar()
