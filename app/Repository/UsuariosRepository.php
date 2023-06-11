9f624989dc4169bc3f5d1e02979aed0c557e21a2
noziXsHooter <nozixco@gmail.com>
2023-06-11 18:58:20 -0300

    add insertUser() method

--- app/Repository/UsuariosRepository.php
+++ app/Repository/UsuariosRepository.php
@@ -19,6 +19,27 @@ class UsuariosRepository
     {
         $this->MySQL = new MySQL();
     }
+
+    /**
+     * Method insertUser.
+     *@param string $login
+     *@param string $senha
+     * @return int
+     */
+    public function insertUser($login, $senha)
+    {
+       $consultaInsert = 'INSERT INTO ' . self::TABELA . ' (login, senha) VALUES (:login, :senha)';
+       $this->MySQL->getDb()->beginTransaction();
+       $stmt = $this->MySQL->getDb()->prepare($consultaInsert);
+       $stmt->bindValue(':login', $login);
+       $stmt->bindValue(':senha', $senha);
+       $stmt->execute();
+
+/*        if ($stmt->errorCode() !== "00000") {
+            $this->MySQL->getDb()->rollback();
+        } */
+       return $stmt->rowCount();
+    }
     
     /**
      * Method getMySQL
