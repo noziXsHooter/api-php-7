a8f13bd773e44d2ffa447d5c4fb549e66f29f758
noziXsHooter <nozixco@gmail.com>
2023-06-20 12:09:48 -0300

    add updateUser method

--- app/Repository/UsuariosRepository.php
+++ app/Repository/UsuariosRepository.php
@@ -40,6 +40,24 @@ class UsuariosRepository
         } */
        return $stmt->rowCount();
     }
+
+    /**
+     * Method updateUser.
+     * @param $id
+     * @param $dados
+     * @return int
+     */
+    public function updateUser($id, $dados)
+    {
+       $consultaUpdate = 'UPDATE ' . self::TABELA . ' SET login = :login, senha = :senha WHERE id = :id';
+       $this->MySQL->getDb()->beginTransaction();
+       $stmt = $this->MySQL->getDb()->prepare($consultaUpdate);
+       $stmt->bindValue(':id', $id);
+       $stmt->bindValue(':login', $dados['login']);
+       $stmt->bindValue(':senha', $dados['senha']);
+       $stmt->execute();
+       return $stmt->rowCount();
+    }
     
     /**
      * Method getMySQL
